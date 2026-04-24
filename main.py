22a5771392aaeca298eab1d621b07af98ee0e7d2
Sergio Cervantes <sergio_germanc@hotmail.com>
2026-04-24 15:17:39 -0600

    Add main file and requirements file for pip install
    Add the main.py to start working in the api information and the
    requirement txt file for keep track of the pip install dependencies.

---
+++ main.py
@@ -0,0 +1,15 @@
+import fastapi
+import uvicorn
+
+api = fastapi.FastAPI()
+
+
+@api.get("/api/calculate")
+def calculate():
+    value = 2 + 2
+
+    result = {"value": value}
+    return result
+
+
+uvicorn.run(api)
