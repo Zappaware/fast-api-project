bb92c64eebaec81df8b1722f4ddd8e6f5353c631
Sergio Cervantes <sergio_germanc@hotmail.com>
2026-04-25 14:50:29 -0600

    Add changes for the real api
    Add files and changes to the work for a real api, not working, api keys
    are missing.

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
-from typing import Optional
+import json
+from pathlib import Path
 
 import fastapi
 import uvicorn
+from starlette.staticfiles import StaticFiles
+
+from api import weather_api
+from services import openweather_service
+from views import home
 
 api = fastapi.FastAPI()
 
 
-@api.get("/")
-def index():
-    body = (
-        "<html>"
-        "<body style='padding: 10px;'>"
-        "<h1>Welcome to the API</h1>"
-        "<div>"
-        "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>"
-        "</div>"
-        "</body>"
-        "</html>"
-    )
-
-    return fastapi.responses.HTMLResponse(content=body)
+def configure():
+    configure_routing()
+    configure_api_keys()
 
 
-@api.get("/api/calculate")
-def calculate(x: int, y: int, z: Optional[int] = None):
-    if z == 0:
-        return fastapi.responses.JSONResponse(
-            content={"error": "ERROR: Z cannot be zero."}, status_code=400
+def configure_api_keys():
+    file = Path("settings.json").absolute()
+    if not file.exists():
+        print(
+            f"WARNING: {file} file not found, you cannot continue, please see settings_template.json"
+        )
+        raise Exception(
+            "settings.json file not found, you cannot continue, please see settings_template.json"
         )
 
-    value = x + y
-
-    if z is not None:
-        value /= z
-
-    return {"x": x, "y": y, "z": z, "value": value}
+    with open(file) as fin:
+        settings = json.load(fin)
+        openweather_service.api_key = settings.get("api_key")
 
 
-# uvicorn was updated, and it's type definitions don't match FastAPI,
-# but the server and code still work fine. So ignore PyCharm's warning:
-# noinspection PyTypeChecker
-uvicorn.run(api, port=8000, host="127.0.0.1")
+def configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
+
+if __name__ == "__main__":
+    configure()
+    # uvicorn was updated, and it's type definitions don't match FastAPI,
+    # but the server and code still work fine. So ignore PyCharm's warning:
+    # noinspection PyTypeChecker
+    uvicorn.run(api, port=8000, host="127.0.0.1")
+else:
+    configure()
