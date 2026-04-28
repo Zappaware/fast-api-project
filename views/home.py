a8661523d72dabe48b3b8ffd39c283a30ad3b5d7
Sergio Cervantes <sergio_germanc@hotmail.com>
2026-04-28 12:47:39 -0600

    Add changes from chapter 7 of the course

--- views/home.py
+++ views/home.py
@@ -1,18 +1,21 @@
 import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
-
 from jinja2 import Environment, FileSystemLoader
+
+from services import report_service
+
 _env = Environment(loader=FileSystemLoader("templates"), cache_size=0)
 templates = Jinja2Templates(env=_env)
 router = fastapi.APIRouter()
 
 
-@router.get("/")
-def index(request: Request):
-    return templates.TemplateResponse(request, "home/index.html")
+@router.get("/", include_in_schema=False)
+async def index(request: Request):
+    events = await report_service.get_reports()
+    return templates.TemplateResponse(request, "home/index.html", {"events": events})
 
 
-@router.get("/favicon.ico")
+@router.get("/favicon.ico", include_in_schema=False)
 def favicon():
     return fastapi.responses.RedirectResponse(url="/static/img/favicon.ico")
